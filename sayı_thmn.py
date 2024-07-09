05e2f9f8e3c9104fc5a5ea5abc2136a1b16249cf
buğra uysal <uysalbugra134@gmail.com>
2024-07-09 04:37:46 +0300

    kaç defada sayıyı  bulduğunu gösteren sayaç eklendi

--- sayı_thmn.py
+++ sayı_thmn.py
@@ -3,12 +3,19 @@ import random
 
 
 def thm():
-    
+    tahinlerim=0
+
     rndd=random.randint(1, 100)
 
     tt=False
-
+  
     while not tt:
+        
+        tahinlerim +=1
+
+
+       
+        
         kullanıcı_sayısı=int(input("tahmin:"))
 
         if kullanıcı_sayısı > rndd:
@@ -18,13 +25,14 @@ def thm():
     
         elif kullanıcı_sayısı < rndd:
             print("daha büyük sayı yazınınz")
+            
 
 
 
         else:
-
             tt=True
             print("doğru bildiniz")    
+            print(tahinlerim)
 
 
 
